3270f6587a3f3871ffda7d6f1265415e43c0edbd
nomad8t <serega18t@tut.by>
2020-09-15 15:26:13 +0300

    feat: trans-arr is solved

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,53 +1,62 @@
-const CustomError = require("../extensions/custom-error");
+const OPERATIONS = ['--discard-next', '--discard-prev', '--double-next', '--double-prev'];
 
 module.exports = function transform(initArr) {
-  if (!Array.isArray(initArr)) {
-    throw Error;
-  } else {
-    let arr = initArr;
-    return chooseOperation(arr);
-  }
+    if (!Array.isArray(initArr)) {
+        throw new Error();
+    } else if (!initArr.length) {
+        return initArr;
+    } else {
+        let arr = initArr.map(el => el);
+        return chooseOperation(arr);
+    }
 };
 
-function doubleNext(arr, ind) {
-    console.log(arr);
-    // return arr.splice(ind, 1, arr[++ind]);
-    return ++ind === arr.length ? arr.splice(--ind, 1) : arr.splice(--ind, 1, arr[++ind]);
+function chooseOperation(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        switch (arr[i]) {
+            case OPERATIONS[0]:
+                discardNext(arr, i);
+                i -= 2;
+                break;
+            case OPERATIONS[1]:
+                discardPrev(arr, i);
+                i--;
+                break;
+            case OPERATIONS[2]:
+                doubleNext(arr, i);
+                i--;
+                break;
+            case OPERATIONS[3]:
+                doublePrev(arr, i);
+                i--;
+                break;
+        }
+    }
+    return arr;
+}
 
+function doubleNext(arr, ind) {
+    if ( OPERATIONS.indexOf(arr[ind + 1]) === -1) {
+        return ind + 1 === arr.length ? arr.splice(ind, 1) : arr.splice(ind, 1, arr[ind + 1]);
+    } else {
+        return arr.splice(ind, 1);
+    }
 }
 
 function doublePrev(arr, ind) {
-    console.log(arr);
-    return --ind < 0 ? arr.splice(++ind, 1) : arr.splice(++ind, 1, arr[--ind]);
+    return ind - 1 < 0 ? arr.splice(ind, 1) : arr.splice(ind, 1, arr[ind - 1]);
 }
 
 function discardNext(arr, ind) {
-    console.log(arr);
-    // return arr.splice(ind, 2);
-    return ++ind === arr.length ? arr.splice(--ind, 1) : arr.splice(--ind, 2);
+    if (ind + 1 === arr.length) {
+        return arr.splice(ind, 1);
+    } else if (arr[ind + 2] === OPERATIONS[1] || arr[ind + 2] === OPERATIONS[3]) {
+        return arr.splice(ind, 3);
+    } else {
+        return arr.splice(ind, 2);
+    }
 }
 
 function discardPrev(arr, ind) {
-    console.log(arr);
-    return --ind < 0 ? arr.splice(++ind, 1) : arr.splice(ind, 2);
-}
-
-function chooseOperation(arr) {
-  const OPERATIONS = ['--discard-next', '--discard-prev', '--double-next', '--double-prev'];
-
-  for (let i = 0; i < arr.length; i++) {
-      console.log(arr);
-      // console.log(OPERATIONS.indexOf(arr[i]));
-      if (OPERATIONS.indexOf(arr[i]) !== -1) {
-          switch (arr[i]) {
-              case OPERATIONS[0]: discardNext(arr, i); break;
-              case OPERATIONS[1]: discardPrev(arr, i); break;
-              case OPERATIONS[2]: doubleNext(arr, i); break;
-              case OPERATIONS[3]: doublePrev(arr, i); break;
-          }
-          i = 0;
-      }
-  }
-  console.log(arr);
-  return arr;
+    return ind - 1 < 0 ? arr.splice(ind, 1) : arr.splice(ind - 1, 2);
 }
